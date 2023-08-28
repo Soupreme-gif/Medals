262bdd53f680ad7c3add32e7eb5990960a3fdb7a
dagas <dshepard3@my.wctc.edu>
2023-08-27 19:16:40 -0500

    Removed unnescessary code

--- src/App.js
+++ src/App.js
@@ -13,32 +13,6 @@ class App extends Component {
     ]
   }
 
-  handleIncrement = (countryId) => {
-
-    const countries = this.state.countries
-
-    for (const c in countries)
-    {
-      console.log(countryId)
-      console.log(c.id)
-       if(c.id === countryId)
-       {
-
-        console.log("this check went off")
-         this.setState({goldMedalCount: c.goldMedalCount + 1})
-       }
-    }
-
-    this.setState({countries:countries})
-    
- }
-
- handleDecrement = (countryId) => {
-
-  this.setState({ goldMedalCount: this.goldMedalCount - 1 })
-
-}
-
   render() {
   return (
     <div className="App">
