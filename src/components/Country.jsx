c6b6766eb6282b43b8a627cc72f47e17a641b1f6
dagas <dshepard3@my.wctc.edu>
2023-09-13 10:15:48 -0500

    New Country as component

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,35 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Medal from './Medal';
+import { TrashFill } from 'react-bootstrap-icons';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-class Country extends Component {
-  getMedalsTotal(country, medals) {
+const Country = (props) => {
+  const { country, medals, onIncrement, onDecrement, onDelete } = props;
+  const getMedalsTotal = (country, medals) => {
     let sum = 0;
     medals.forEach(medal => { sum += country[medal.name]; });
     return sum;
   }
-  render() { 
-    const { country, medals, onIncrement, onDecrement, onDelete } = this.props;
-    return (
-      <div className="country">
-        <div className="name">
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title className="d-flex justify-content-between">
+          <span>
           { country.name }
-          <span className="badge">
-            { this.getMedalsTotal(country, medals) }
+          <Badge bg="secondary" text="light" pill className="ml-2">
+            { getMedalsTotal(country, medals) }
+          </Badge>
           </span>
-        </div>
+          <TrashFill onClick={() => onDelete(country.id)} className='icon-btn' style={{ color:'red' }} />
+        </Card.Title>
+        <ListGroup variant="flush">
         { medals.map(medal =>
-          <Medal 
-            key={ medal.id } 
+        <ListGroup.Item className="d-flex justify-content-between" key={ medal.id }>
+          <Medal  
             country={ country } 
             medal={ medal } 
             onIncrement={ onIncrement } 
             onDecrement={ onDecrement } />
+        </ListGroup.Item>
         ) }
-        <button onClick={() => onDelete(country.id)}>delete</button>
-        <hr />
-      </div>
-    );
-  }
+        </ListGroup>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default Country;
--- src/components/NewCountry.jsx
+++ src/components/NewCountry.jsx
@@ -1,20 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
+import { PlusCircleFill } from 'react-bootstrap-icons';
 
-class NewCountry extends Component {
-  state = {  }
-  handleClick = () => {
-    const name = prompt('Enter country name');
-    if (name && name.trim().length > 0) {
-      this.props.onAdd(name);
-    }
+const NewCountry = (props) => {
+  const [ showModal, setShowModal ] = useState(false);
+  const [ showToast, setShowToast ] = useState(false);
+  const [ newCountryName, setNewCountryName ] = useState("");
+
+  const handleModalClose = () => setShowModal(false);
+  const handleModalShow = () => {
+    setShowModal(true);
+    setNewCountryName("");
   }
-  render() { 
-    return (
-      <div className='newCountryButton'>
-        <button onClick={ this.handleClick }>New Country</button>
-      </div>
-    );
+  const handleModalKeyPress = (e) => (e.keyCode ? e.keyCode : e.which) === 13 && handleAdd();
+  const handleAdd = () => {
+    newCountryName.length > 0 ? props.onAdd(newCountryName) : setShowToast(true);
+    handleModalClose();
   }
+
+  return (
+    <React.Fragment>
+      <Button variant="outline-success" onClick={ handleModalShow }>
+        <PlusCircleFill />
+      </Button>
+      <Modal onKeyPress={ handleModalKeyPress } show={ showModal } onHide={ handleModalClose }>
+        <Modal.Header closeButton>
+          <Modal.Title>New Country</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group className="mb-3" controlId="modalForm1">
+            <Form.Label>Country Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="newCountryName"
+              onChange={ (e) => setNewCountryName(e.target.value) }
+              value={ newCountryName }
+              placeholder="Enter Name"
+              autoFocus
+              autoComplete='off'
+            />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={ handleModalClose }>
+            Close
+          </Button>
+          <Button variant="primary" onClick={ handleAdd }>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <ToastContainer className="position-fixed" style={{top:5,right:5,zIndex:5,width:'18rem'}}>
+      <Toast show={ showToast } onClose={ () => setShowToast(false) } delay={ 2000 } autohide>
+        <Toast.Header className="justify-content-between">
+          <strong className="me-auto">Country Not Added</strong>
+        </Toast.Header>
+        <Toast.Body>Enter a name to add a new country</Toast.Body>
+      </Toast>
+      </ToastContainer>
+    </React.Fragment>
+  );
 }
 
 export default NewCountry;
